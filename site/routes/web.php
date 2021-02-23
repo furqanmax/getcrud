4d9d30d3beaa09d32f9b2cd9a17df2aec872d90f
furqanmax <furqanmax27@gmail.com>
2021-02-23 07:52:30 +0530

    routes

--- site/routes/web.php
+++ site/routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
 Route::get('/visitormakecontrollerserver', 'VisitorController@makecontrollerserver');
 
-Route::get('/playground', function () {
+Route::get('/', function () {
     return view('visitorsplayground');
 });
 
