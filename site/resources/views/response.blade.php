8fbf2109a8e15b7a69ac3d780601ee2695732e14
furqanmax <furqanmax27@gmail.com>
2021-02-26 18:04:53 +0530

    mail updates

--- site/resources/views/response.blade.php
+++ site/resources/views/response.blade.php
@@ -3,8 +3,11 @@
 <head>
 	<title>Thank</title>
 </head>
-<body style="background: red;color: white">
+<body style="">
 	<h5>Thank you {{$user['name']}}</h5>
 	<p>Thank you for contacting us. We hope you are enjoying getCRUD. We would love to here more from you. </p>
+
+	Thanking you,
+	Team GetCRUD
 </body>
 </html>
